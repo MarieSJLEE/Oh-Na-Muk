8cb3eba30e3fb129fd7b91843ec90bcb0eca7555
MarieSJLEE <marie.sjlee@gmail.com>
2023-12-13 01:11:46 +0900

    구조 추가, MoodCheck 추가

---
+++ src/compoments/FoodInput.js
@@ -0,0 +1,28 @@
+// src/components/FoodInput.js
+import React, { useState } from 'react';
+
+const FoodInput = ({ onFoodSubmit }) => {
+  const [food, setFood] = useState('');
+
+  const handleFoodSubmit = () => {
+    onFoodSubmit(food);
+    setFood('');
+  };
+
+  return (
+    <div>
+      <h1>Food Input</h1>
+      <label>
+        What did you eat?{' '}
+        <input
+          type="text"
+          value={food}
+          onChange={(e) => setFood(e.target.value)}
+        />
+      </label>
+      <button onClick={handleFoodSubmit}>Submit</button>
+    </div>
+  );
+};
+
+export default FoodInput;
