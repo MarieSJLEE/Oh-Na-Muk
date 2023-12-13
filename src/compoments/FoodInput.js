f80cba50ef76fc0ec680cb044591748f245a17a4
MarieSJLEE <marie.sjlee@gmail.com>
2023-12-13 13:33:54 +0900

    Food Input 엑셀 데이터 삽입 완료

--- src/compoments/FoodInput.js
+++ src/compoments/FoodInput.js
@@ -1,12 +1,33 @@
 // src/components/FoodInput.js
-// git 정리
 import React, { useState } from 'react';
 
 const FoodInput = ({ onFoodSubmit }) => {
   const [food, setFood] = useState('');
 
-  const handleFoodSubmit = () => {
-    onFoodSubmit(food);
+  const handleFoodSubmit = async () => {
+    // 파싱된 음식 데이터를 배열로 변환
+    const foodArray = food.split(',').map(item => item.trim());
+    
+    try {
+      const response = await fetch('http://192.168.0.2:3001/saveData', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ food: foodArray }),
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        console.log('Data saved successfully!');
+        // 추가적인 처리나 화면 갱신이 필요하다면 여기서 처리
+      }
+    } catch (error) {
+      console.error('Error saving data:', error);
+    }
+
+    // 입력된 음식 초기화
     setFood('');
   };
 
